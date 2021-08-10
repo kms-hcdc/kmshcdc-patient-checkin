f7e50e79c819e4cd1e80e89d19e2858a70527640
Longtdo <longdt911@gmail.com>
2021-08-10 17:42:34 +0700

    Complete View Patient In List

--- src/PatientChecking/PatientChecking/Services/PatientService.cs
+++ src/PatientChecking/PatientChecking/Services/PatientService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using PatientChecking.Services.Repository;
 using PatientChecking.Services.ServiceModels;
+using PatientChecking.Services.ServiceModels.Enum;
 using PatientChecking.Views.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -11,26 +12,26 @@ namespace PatientChecking.Services
 {
     public class PatientService : IPatientService
     {
-        private readonly PatientCheckInContext patientCheckInContext;
+        private readonly PatientCheckInContext _patientCheckInContext;
 
         public PatientService(PatientCheckInContext patientCheckInContext)
         {
-            this.patientCheckInContext = patientCheckInContext;
+            _patientCheckInContext = patientCheckInContext;
         }
 
-        public async Task<PagedResult<PatientListViewModel>> GetListPatientPaging(PagingRequest request)
+        public PatientList GetListPatientPaging(PagingRequest request)
         {
-            var query = from patient in patientCheckInContext.Patients
-                        join contact in patientCheckInContext.Contacts on patient.PatientId equals contact.PatientId
-                        join address in patientCheckInContext.Addresses on contact.ContactId equals address.ContactId
+            var query = from patient in _patientCheckInContext.Patients
+                        join contact in _patientCheckInContext.Contacts on patient.PatientId equals contact.PatientId
+                        join address in _patientCheckInContext.Addresses on contact.ContactId equals address.ContactId
                         where address.IsPrimary == true
                         select new { patient, contact, address };
 
-            if(request.SortSelection == 0)
+            if (request.SortSelection == (int)PatientSortSelection.ID)
             {
                 query = query.OrderBy(i => i.patient.PatientIdentifier);
             }
-            else if(request.SortSelection == 1)
+            else if (request.SortSelection == (int)PatientSortSelection.Name)
             {
                 query = query.OrderBy(i => i.patient.FullName);
             }
@@ -40,39 +41,36 @@ namespace PatientChecking.Services
             }
 
             int totalRow = query.Count();
-            var data = await query.Skip((request.PageIndex - 1) * request.PageSize)
-                .Take(request.PageSize).Select(x => new PatientListViewModel()
+            var data = query
+                .Skip((request.PageIndex - 1) * request.PageSize)
+                .Take(request.PageSize).Select(x => new Patient()
                 {
+                    PatientId = x.patient.PatientId,
                     PatientIdentifier = x.patient.PatientIdentifier,
                     FullName = x.patient.FullName,
-                    DoB = x.patient.DoB.ToString("dd-MM-yyyy"),
-                    Gender = x.patient.Gender == 0 ? "Male" : x.patient.Gender == 1 ? "Female" : "Others",
-                    Address = x.address != null ? x.address.Address1 : "",
-                    PhoneNumber = x.contact != null ? x.contact.PhoneNumber : "",
-                    Email = x.contact != null ? x.contact.Email : "",
-                    AvatarLink = x.patient.AvatarLink != null ? x.patient.AvatarLink : ""
-                }).ToListAsync();
-
-            var pagedResult = new PagedResult<PatientListViewModel>()
+                    DoB = x.patient.DoB,
+                    Gender = x.patient.Gender,
+                    AvatarLink = x.patient.AvatarLink != null ? x.patient.AvatarLink : "",
+                    PrimaryAddress = x.address,
+                    PrimaryContact = x.contact,
+                }).ToList();
+            PatientList result = new PatientList()
             {
-                Items = data,
-                TotalCount = totalRow,
-                pageIndex = request.PageIndex,
-                pageSize = request.PageSize
+                Patients = data,
+                TotalCount = totalRow
             };
-
-            return pagedResult;
+            return result;
         }
 
         public async Task<int> GetNumberOfPatients()
         {
-            var result = await patientCheckInContext.Patients.ToListAsync();
+            var result = await _patientCheckInContext.Patients.ToListAsync();
             return result.Count;
         }
 
         public async Task<int> GetNumberOfPatientsInCurrentMonth()
         {
-            var result = await patientCheckInContext.Appointments.Where(x => x.CheckInDate.Month == DateTime.Now.Month)
+            var result = await _patientCheckInContext.Appointments.Where(x => x.CheckInDate.Month == DateTime.Now.Month)
                                                                  .GroupBy(p => p.PatientId)
                                                                  .Select(g => new { g.Key, count = g.Count()}).ToListAsync();
             return result.Count;
--- src/PatientChecking/PatientChecking/Services/Repository/IPatientService.cs
+++ src/PatientChecking/PatientChecking/Services/Repository/IPatientService.cs
@@ -11,6 +11,6 @@ namespace PatientChecking.Services.Repository
     {
         Task<int> GetNumberOfPatients();
         Task<int> GetNumberOfPatientsInCurrentMonth();
-        Task<PagedResult<PatientListViewModel>> GetListPatientPaging(PagingRequest request);
+        PatientList GetListPatientPaging(PagingRequest request);
     }
 }

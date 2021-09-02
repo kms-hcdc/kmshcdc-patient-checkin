1b0e310bcbc0d90afe94aeef6f7e9a2f207e73d5
Phien Le <phienle@kms-technology.com>
2021-09-03 00:23:23 +0700

    Fix unit tests

--- src/PatientChecking/PatientCheckIn.Tests/Controllers/AppointmentControllerTests.cs
+++ src/PatientChecking/PatientCheckIn.Tests/Controllers/AppointmentControllerTests.cs
@@ -15,7 +15,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Web.Mvc;
 using Xunit;
 
 namespace PatientCheckIn.Tests.Controllers
